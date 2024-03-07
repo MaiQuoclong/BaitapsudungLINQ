cf1e3421cc2eb3b7e2f1afe021308f8cd6038882
MaiQuoclong <162609576+MaiQuoclong@users.noreply.github.com>
2024-03-07 23:26:23 +0700

    Add files via upload

---
+++ Truck.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Truck : Vehicle
+{
+    public string Company { get; set; }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Vehicle
+{
+    public string Brand { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+}
